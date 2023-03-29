dcf673f3360351d9b8e8f18823adea24c5966b3c
unknown <momen07996@gmail.com>
2023-03-29 17:37:48 +0300

    our work

--- bootstrap-5.2.2-dist/js/index.js
+++ bootstrap-5.2.2-dist/js/index.js
@@ -3,8 +3,19 @@ for (let i = 0; i < tag_header.length; i++) {
     var chekd = tag_header[i];
     chekd.addEventListener("click", function () {
         for (let i = 0; i < tag_header.length; i++) {
-            tag_header[i].classList.remove("active");
+            tag_header[i].classList.remove("active1");
         }
-        this.classList.add("active");
+        this.classList.add("active1");
+    });
+}
+
+var div_our = document.querySelectorAll(".div_our");
+for (let i = 0; i < div_our.length; i++) {
+    var chekd = div_our[i];
+    chekd.addEventListener("click", function () {
+        for (let i = 0; i < div_our.length; i++) {
+            div_our[i].classList.remove("active2");
+        }
+        this.classList.add("active2");
     });
 }

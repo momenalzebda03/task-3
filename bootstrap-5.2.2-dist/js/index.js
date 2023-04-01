0db3f8eee652c3811f6bf02a5fb64dd816ca2774
unknown <momen07996@gmail.com>
2023-04-01 19:00:11 +0300

    create facts and lorem

--- bootstrap-5.2.2-dist/js/index.js
+++ bootstrap-5.2.2-dist/js/index.js
@@ -30,3 +30,26 @@ for (let i = 0; i < div_back.length; i++) {
         this.classList.add("active3");
     });
 }
+
+let nums = document.querySelectorAll(".nums .num");
+let section = document.querySelector(".three");
+let started = false;
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 80000 / goal);
+}
